68f2c3e503704dd141e60e98137b0476a561bdc6
Cs_Szabj04 <68150404+CsikSzabi04@users.noreply.github.com>
2026-03-05 11:26:03 +0100

    Add files via upload

---
+++ 2/2/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _2{
+
+    struct Allomas{
+        public int kod;
+        public string nev;
+        public double atlag;
+        public int meresszam;
+    }
+    internal class Program{
+        static void Main(string[] args){
+            string[] sor;
+
+            Console.Write("Fajl tartalma: \n");
+            try{
+                sor = File.ReadAllLines("..\\..\\..\\adatok.txt");
+            } catch (Exception exc){
+                Console.WriteLine("Problema!\n");
+                return;
+            }
+
+            foreach (var s in sor) Console.WriteLine(s);
+            Console.WriteLine();
+
+            Allomas[] allomas = new Allomas[sor.Length];
+
+            for (int i = 1; i < sor.Length; i++) {
+                string s = sor[i];
+                string[] reszek = s.Split(' ');
+                allomas[i-1].kod = int.Parse(reszek[0].Substring(1));
+                allomas[i-1].nev = reszek[1].Replace('_',' ');
+                allomas[i-1].meresszam = reszek.Length- 2;
+                allomas[i-1].atlag = 0;
+                for (int a = 2; a < reszek.Length; a++) allomas[i-1].atlag += double.Parse(reszek[a], System.Globalization.NumberStyles.Any, new System.Globalization.CultureInfo("hu-HU"));
+                allomas[i-1].atlag /= allomas[i-1].meresszam;
+
+
+
+            }
+
+        }
+
+    }
+}
---
+++ eugdp/eugdp/Program.cs
@@ -0,0 +1,19 @@
+﻿
+namespace eugdp
+{
+    struct GPD{
+        public int varos;
+        public string gpd;
+    }
+    internal class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Hello, World!");
+            Beolvas("..\\..\\..\\adatok.txt",out string[] orszagok, out double[]GPDnov);
+        }
+
+        private static void Beolvas(string v, out string[] orszagok, out double[] gPDnov)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
