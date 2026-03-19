096abac00d27c650e2a953bd030753cf9e086eb5
Cs_Szabj04 <68150404+CsikSzabi04@users.noreply.github.com>
2026-03-19 11:09:12 +0100

    Add files via upload

---
+++ 4/kikoto/ca/Kikoto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ca{
+    internal class Kikötő{
+
+        public int[] Hajok = { 0,0,5,2,11,4,5,3,7,7,9,10};           
+        public int LépesekSzáma;
+        Random rand = new Random();
+
+        public void Mutat(){for (int i = 2; i < 12; i++) Console.WriteLine($"{i}: "+new string('H', Hajok[i]));}
+
+        /// <summary>
+        /// Visszaadja a kikötőben levő hajók számát
+        /// </summary>
+        public int HajokSzama() {
+            int ÖsszHajó = 0;
+            for(int i = 2; i < Hajok.GetLength(0); i++) ÖsszHajó += Hajok[i];  
+            return ÖsszHajó;
+        }
+
+        public void Sorsol(){
+            int kocka1 = rand.Next(1,7);
+            int kocka2 = rand.Next(1,7);
+            int index = kocka1 + kocka2;
+            if (Hajok[index] > 0) Hajok[index]--;
+            LépesekSzáma++;
+        }
+    }
+}
---
+++ 4/kikoto/ca/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ca{
+    internal class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Kikötő szimulátor");
+            Kikötő k = new Kikötő();
+            k.Mutat();
+            Console.WriteLine("Hajó száma a kikötőben: " + $"{k.HajokSzama()}"); Console.WriteLine("");
+
+            while (k.HajokSzama() > 0){
+                Console.WriteLine("\nPress space to continue!");
+                Console.ReadKey();
+                k.Sorsol();
+                k.Mutat();
+                Console.WriteLine("Hajó száma a kikötőben: " + $"{k.HajokSzama()}");
+                Console.WriteLine("Lépések száma: " + $"{k.LépesekSzáma}");
+                
+            }
+        }
+    }
+}
